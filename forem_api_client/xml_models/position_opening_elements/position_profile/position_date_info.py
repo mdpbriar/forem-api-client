b7081d8609947abecf4ecfdbf7ae75c59112a1b7
Arthur Briquet <mdpbriar@henallux.be>
2025-05-22 17:08:18 +0200

    fix: correct typo that failed to print correctly StartAsSoonAsPossible

--- forem_api_client/xml_models/position_opening_elements/position_profile/position_date_info.py
+++ forem_api_client/xml_models/position_opening_elements/position_profile/position_date_info.py
@@ -7,4 +7,4 @@ class PositionDateInfo(BaseXmlModel, tag='PositionDateInfo', skip_empty=True):
 
     start_date: Optional[date] = element(tag='StartDate', default=None)
     expected_end_date: Optional[date] = element(tag='ExpectedEndDate', default=None)
-    as_soon_as_possible: Optional[bool] = element(tage='StartAsSoonAsPossible', default=None)
+    as_soon_as_possible: Optional[bool] = element(tag='StartAsSoonAsPossible', default=None)
