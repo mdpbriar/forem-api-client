2964ecc4468485317e5a94d1602847ea9ac6b45b
Arthur Briquet <mdpbriar@henallux.be>
2025-06-23 11:50:46 +0200

    feat: dimeco filter accept None

--- forem_api_client/nomenclatures_models/dimeco.py
+++ forem_api_client/nomenclatures_models/dimeco.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from forem_api_client.nomenclatures_models.base import BaseNomenclature
 
 
@@ -5,11 +7,14 @@ class Dimeco(BaseNomenclature):
 
     file = 'dimeco'
 
-    def get_filtered_dataframe(self, romev3_id: str):
+    def get_filtered_dataframe(self, romev3_id: str|None = None) -> pd.DataFrame:
         df = self.get_dataframe()
-        return df[df['id'].str.startswith(romev3_id, na=False)]
+        if romev3_id:
+            df = df[df['id'].str.startswith(romev3_id, na=False)]
+        return df
 
-    def get_filtered_options(self, romev3_id: str):
+
+    def get_filtered_options(self, romev3_id: str|None = None):
         df = self.get_filtered_dataframe(romev3_id)
         return df.to_dict(orient='records')
 
